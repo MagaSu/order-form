a05dfe5124bb463fe212cc12239577eae9ac76e4
MagaSu <magamag268@gmail.com>
2021-10-25 16:46:53 +0200

    Some changes in index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+//this line makes PHP behave in a more strict way
+declare(strict_types=1);
+
+//we are going to use session variables so we need to enable sessions
+session_start();
+
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+// function validate(){
+    
+// }
+
+
+//your products with their price.
+$products = [
+    ['name' => 'Club Ham', 'price' => 3.20],
+    ['name' => 'Club Cheese', 'price' => 3],
+    ['name' => 'Club Cheese & Ham', 'price' => 4],
+    ['name' => 'Club Chicken', 'price' => 4],
+    ['name' => 'Club Salmon', 'price' => 5]
+];
+
+$products = [
+    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Fanta', 'price' => 2],
+    ['name' => 'Sprite', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 3],
+];
+
+$totalValue = 0;
+
+require 'form-view.php';
+
+// whatIsHappening();
+
+if (!empty($_POST['email']) && !empty($_POST['street']) && !empty($_POST['streetnumber']) && !empty($_POST['city']) && !empty($_POST['zipcode'])){
+    if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+        $email = $_POST['email'];
+        echo $email;
+    }
+    if(filter_var($_POST['streetnumber'], FILTER_VALIDATE_INT)){
+        $streetnumber = $_POST['streetnumber'];
+        echo $streetnumber;
+    }
+    if(filter_var($_POST['zipcode'], FILTER_VALIDATE_INT)){
+        $zipcode = $_POST['zipcode'];
+        echo $zipcode;
+    }
+    $street = $_POST['street'];
+    $city = $_POST['city'];
+
+    echo $street;
+    echo $city;
+}
